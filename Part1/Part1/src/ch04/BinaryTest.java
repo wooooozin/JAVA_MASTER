6945d48361c815b8ab0a99d87e974483b329437c
wooooozin <wooooozin@gmail.com>
2023-03-27 05:11:23 +0900

    컴퓨터에서 자료 표현하기

---
+++ Part1/Part1/src/ch04/BinaryTest.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		
+		int bNum = 0B1010;
+		int oNum = 012;
+		int xNum = 0XA;
+		
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+
+		
+
+	}
+
+}
