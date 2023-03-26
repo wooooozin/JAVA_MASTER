4bc73aed9234ff7a0074adc6b98cf847af520b64
wooooozin <wooooozin@gmail.com>
2023-03-27 05:17:23 +0900

    변수 선언 및 사용

---
+++ Part1/Part1/src/ch05/VariableTest.java
@@ -0,0 +1,17 @@
+package ch05;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+
+		int age, count;
+		age = 20;
+		count = 3;
+		
+		int level = 10000;
+		System.out.println(age);
+		System.out.println(level);
+		
+	}
+
+}
