f7e23a148a658cf299fa53b5debc0a4550ed84ce
wooooozin <wooooozin@gmail.com>
2023-03-27 06:04:47 +0900

    논리형 데이터 타입과 지역 변수 타입 추론

---
+++ Part1/Part1/src/ch09/BooleanTest.java
@@ -0,0 +1,19 @@
+package ch09;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+
+		boolean isTrue = false;
+		System.out.println(isTrue);
+		
+		if (isTrue == true) {
+			System.out.println("참 입니다.");
+		}
+		else {
+			System.out.println("거짓 입니다.");
+		}
+		
+	}
+
+}
