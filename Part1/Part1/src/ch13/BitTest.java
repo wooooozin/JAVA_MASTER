110a4655d151110e3539ec27e92a57d5e305f8a7
wooooozin <wooooozin@gmail.com>
2023-03-28 05:48:20 +0900

    연산자 3

--- Part1/Part1/src/ch13/BitTest.java
+++ Part1/Part1/src/ch13/BitTest.java
@@ -7,15 +7,15 @@ public class BitTest {
 		int num1 = 5;  	// 00000101
 		int num2 = 10; 	// 00001010
 				
-		System.out.println(num1 | num2);
-		System.out.println(num1 & num2);
-		System.out.println(num1 ^ num2);
-		System.out.println(~num1);
+		System.out.println(num1 | num2); // 15
+		System.out.println(num1 & num2); // 0
+		System.out.println(num1 ^ num2); // 15
+		System.out.println(~num1); // -6
 		
-		System.out.println(num1 << 2);
-		System.out.println(num1);
-		System.out.println(num1 <<= 2);
-		System.out.println(num1);
+		System.out.println(num1 << 2); // 20
+		System.out.println(num1); // 5
+		System.out.println(num1 <<= 2); // 20
+		System.out.println(num1); // 20
 		
 	}
 
